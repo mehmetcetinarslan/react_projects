e8e0ab9094675bafd058ed8fc48db8ab5e225934
mehmetcetinarslan <mhmtcet@gmail.com>
2023-11-07 16:18:15 +0300

    ilk gün (7kasim) çalışmaları
    7kasim_cetinarslan

---
+++ reacts_7kasim/src/App.js
@@ -0,0 +1,72 @@
+
+import './App.css';
+
+
+function App() {
+  return(
+    <div>
+<div class="mx-auto">
+<div class="">
+      <h1>Anasayfa Logo</h1>
+       <img src="" alt="" />
+    </div>
+    <div >
+      
+        <span class="px-2"><a href="">Anasayfa</a></span>
+        <span class="px-2"><a href="">Galeri</a></span>
+        <span class="px-2"><a href="">Ürünler</a></span>
+        <span class="px-2"><a href="">İletişim</a></span>
+
+      
+    </div>
+</div>
+<hr />
+
+       
+    <div>
+      <p>Kategoriler</p>
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+
+      </ul>
+      </div>
+      <div >
+        <div class="">
+            <img src="react-course/public/logo192.png"/>
+            <h3>Ürün 1</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <a href=""></a>
+        </div>
+        <div class="">
+            <img src="react-course/public/logo192.png"/>
+            <h3>Ürün 2</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <a href=""></a>
+        </div>
+        <div class="">
+            <img src="react-course/public/logo192.png"/>
+            <h3>Ürün 3</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <a href=""></a>
+        </div>
+        <div class="">
+            <img src="react-course/public/logo192.png"/>
+            <h3>Ürün 4</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <a href=""></a>
+        </div>
+        </div>
+
+    
+    
+    </div>
+  );
+
+ 
+}
+
+
+export default App;
