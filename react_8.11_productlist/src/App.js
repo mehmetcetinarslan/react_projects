6bb581ca494a826fa67d56a65e34c2c711d6363d
mehmetcetinarslan <mhmtcet@gmail.com>
2023-11-08 13:44:23 +0300

    8.11 react product list
    ürün listesinde ürün detaylarını gösterip kapatan ekle ve kapat butonları ile yapıldı.

---
+++ react_8.11_productlist/src/App.js
@@ -0,0 +1,117 @@
+
+import './App.css';
+
+
+function App() {
+  return(
+    <div>
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#CCF0F4" }}> 
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/1/11/SNC_logo.svg" width="350" /></a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Anasayfa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kategoriler</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ürünler</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">İletişim</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+    <div class="container mt-4">
+    
+        <div class="row">
+            <div class="col-md-3">
+            <h2 class="mb-4">Kategoriler</h2>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                    <a href="#" class="list-group-item list-group-item-action">Kategori</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+            <h2 class="mb-4">Ürünler</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="card-img-top"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Ürün</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    </div>
+  );
+
+ 
+}
+
+
+export default App;
